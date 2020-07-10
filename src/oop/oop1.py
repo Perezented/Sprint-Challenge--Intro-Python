0da81d51b8c2bdd2eeeef402dddf2df1f31e5acf
Michael Perez <mperez6473@gmail.com>
2020-07-10 10:37:57 -0500

    Filled out the classes in oop1 but all the test fail. Mentioning I left out 2 required arguments. Name and vehicleType?

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -21,21 +21,40 @@
 
 # looks like a VEHICLE is the main parent class for all of the following classes
 class Vehicle:
-    def __init__(self, name, vType):
+    def __init__(self, name, vehicleType):
         self.name = name
-        self.vType = vType
+        self.vehicleType = vehicleType
 # FLIGHTVEHICLE fom the main class, Vehicle
+class FlightVehicle(Vehicle):
+    def __init__(self, name, vehicleType):
+        super().__init__(name, vehicleType)
 
 
 # which in turn has two subclasses below that
 # STARSHIP branched off of FlightVehicle
-
+class Starship(FlightVehicle):
+    def __init__(self, name, vehicleType, spaceshipType):
+        super().__init__(name, vehicleType)
+        self.spaceshipType = spaceshipType
 # AIRPLANE branched off of FlightVehicle
-
+class Airplane(FlightVehicle):
+    def __init__(self, name, vehicleType,  airplaneType):
+        super().__init__(name, vehicleType)
+        self.airplaneType = airplaneType
 
 # GROUNDVEHICLE branches off of the main branched
-
+class GroundVehicle(Vehicle):
+    def __init__(self, name, vehicleType):
+        super().__init__(name, vehicleType)
 # Car and Motorcycle branch off of a GroundVehicle
 # CAR
+class Car(GroundVehicle):
+    def __init__(self, name, vehicleType, carType):
+        super().__init__(name, vehicleType)
+        self.carType = carType
 
 # MOTORCYCLE
+class Car(GroundVehicle):
+    def __init__(self, name, vehicleType, motorcycleType):
+        super().__init__(name, vehicleType)
+        self.motorcycleType = motorcycleType
