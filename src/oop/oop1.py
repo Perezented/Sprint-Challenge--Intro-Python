9901973127d6b59f9b8008c6c84b74009a8c9a66
Michael Perez <mperez6473@gmail.com>
2020-07-10 10:18:25 -0500

    Started by writing out comments and have the first vehicle class typed out.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,25 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# looks like a VEHICLE is the main parent class for all of the following classes
+class Vehicle:
+    def __init__(self, name, vType):
+        self.name = name
+        self.vType = vType
+# FLIGHTVEHICLE fom the main class, Vehicle
+
+
+# which in turn has two subclasses below that
+# STARSHIP branched off of FlightVehicle
+
+# AIRPLANE branched off of FlightVehicle
+
+
+# GROUNDVEHICLE branches off of the main branched
+
+# Car and Motorcycle branch off of a GroundVehicle
+# CAR
+
+# MOTORCYCLE
